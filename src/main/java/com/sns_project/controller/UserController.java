98aa3d6435efeb8ba713b9e584c657b55de34135
MetaDatamong <137029208+MetaDatamong@users.noreply.github.com>
2024-10-15 10:56:42 +0900

    [friend 기능 구현중 중]
    프로필 주인, 손님 구별하여 프로필에 버튼 추가.

--- src/main/java/com/sns_project/controller/UserController.java
+++ src/main/java/com/sns_project/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.sns_project.controller;
 
+import com.sns_project.config.auth.PrincipalDetails;
 import com.sns_project.domain.User;
 import com.sns_project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class UserController {
 
@@ -17,11 +21,26 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping("/user/{userId}")
-    public String getProfile(@AuthenticationPrincipal UserDetails userDetails, @PathVariable Long userId, Model model) {
+    public String getProfile(@AuthenticationPrincipal PrincipalDetails userDetails, @PathVariable Long userId, Model model) {
 
         model.addAttribute("principal", userDetails);
         User user = userRepository.findById(userId).orElse(null);
         model.addAttribute("user", user);
+        // 해당 프로필 주인인지 확인하는 로직 추가
+            // 해당 프로필이 주인인지 확인.
+            // 맞다면 콜렉션으로 만들어서 isOwner라는 객체를 user와 함께 담아서 model로 보내기.
+
+        Map<String, Object> userInfo = new HashMap<>();
+
+        boolean isOwner = false;
+        if (user != null && userDetails != null && userDetails.getUser() != null) {
+            isOwner = userDetails.getUserId().equals(user.getUserId());
+        }
+
+        userInfo.put("user", user);
+        userInfo.put("isOwner", isOwner);
+
+        model.addAttribute("userInfo", userInfo);
 
         return "user/profile";
 
---
+++ src/main/java/com/sns_project/dto/FriendDto.java
@@ -0,0 +1,5 @@
+package com.sns_project.dto;
+
+
+public class FriendDto {
+}
